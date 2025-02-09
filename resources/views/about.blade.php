152b599a2f1e2191b0b530b8267330d1a2f9cac8
Ivan Trapero <ivantrapero123@gmail.com>
2025-02-09 15:45:16 +0800

    0209_Vid4_make-it-pretty-using-tailwindcss

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Page</title>
-</head>
-<body>
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
     <h1>Hello from the About Page.</h1>
-</body>
-</html>
+</x-layout>
